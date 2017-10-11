93fe380fe6da6cdbc5645f99aedbedbee69d5a2c
Daniel Alexandre <liengesbor@gmail.com>
2017-10-10 17:54:14 -0700

    Moved files for prod

--- app.php
+++ app.php
@@ -3,10 +3,10 @@
 use Symfony\Component\HttpFoundation\Request;
 
 /** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = require __DIR__.'/app/autoload.php';
+include_once __DIR__.'/app/bootstrap.php.cache';
 
-$kernel = new AppKernel('prod', false);
+$kernel = new AppKernel('prod', true);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
